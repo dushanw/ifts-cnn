8d1e7da528febefafec2e7b6bdd7ab009ff99f13
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-06-06 17:20:25 +0530

    added the wrapper function for PSF submodule

---
+++ f_simPSFs.m
@@ -0,0 +1,24 @@
+% 2021-06-06 by Dushan N. Wadduwage
+% A wrapper to call the optical_PSF subtoolbox
+
+function PSFs = f_simPSFs(pram)
+
+  cd('_submodules/optical_PSF/');
+  APSF_3D     = Efficient_PSF(pram.NA, pram.nm, pram.lambda_ex, pram.dx,pram.Nx-2,pram.Nx-2,2,200);
+  PSF_3D      = abs(APSF_3D{1}).^2+abs(APSF_3D{2}).^2+abs(APSF_3D{3}).^2;
+  exPSF       = PSF_3D(:,:,2).^2; % 2021-04-13 check with Peter if this dependence is correct. 
+  exPSF       = exPSF/sum(exPSF(:));
+  
+  APSF_3D     = Efficient_PSF(pram.NA, pram.nm, pram.lambda_em, pram.dx,pram.Nx-2,pram.Nx-2,2,200);
+  PSF_3D      = abs(APSF_3D{1}).^2+abs(APSF_3D{2}).^2+abs(APSF_3D{3}).^2;
+  emPSF       = PSF_3D(:,:,2);
+  emPSF       = emPSF/sum(emPSF(:));
+  cd(of);
+    
+  %% save PSFs
+  PSFs.exPSF  = exPSF;
+  PSFs.emPSF  = emPSF;  
+  PSFs.pram   = mcls_pram;
+        
+  save(['./_PSFs' datestr(datetime('now')) '.mat'],'PSFs','-v7.3'); % save sPSF
+end
