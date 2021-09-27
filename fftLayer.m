101e9a4c5248ee17858f2f1d4007e21a166eebc2
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-09-27 06:57:12 -0700

    Expanded the main code for more experimetns (needs cleanup). Minor fixes to fftLayer, and f_gen_inv layers. Implemented a new f_gen_inv_rf forrandomly selected fixed sampling case.

--- fftLayer.m
+++ fftLayer.m
@@ -2,6 +2,7 @@ classdef fftLayer < nnet.layer.Layer
 
   properties
     Nk
+    visIdx
   end
 
   properties (Learnable)
@@ -10,13 +11,15 @@ classdef fftLayer < nnet.layer.Layer
   methods
     function layer = fftLayer(name,k)    
       layer.Nk     = length(k);
+      layer.visIdx = find(2*pi./k>300 & 2*pi./k<800)+1;
       layer.Name   = name;
     end
 
     function X = predict(layer,X)
       % X will have input dimentions [y x opd b]
       X   = abs(fft(X,[],3));
-      X   = X(:,:,2:layer.Nk+1,:);
+      % X   = X(:,:,2:layer.Nk+1,:);
+      X   = X(:,:,layer.visIdx,:);
     end
     
   end
